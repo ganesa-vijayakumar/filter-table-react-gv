da61018b626bff16a9c7db73a16e84786d327c85
Ganesa Vijayakumar <ganeshnet.vgp@gmail.com>
2021-06-30 22:52:49 +0530

    - Updated the app with material UI
    - Displayed all the information of the Mockdata.json
    - Used core and data-grid of material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import data from './MOCK_DATA.json';
 import { useState } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
 
 function App() {
   const [filteredData, setFilteredData] = useState([]);
@@ -21,6 +22,41 @@ function App() {
     }
   };
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    {
+      field: 'first_name',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'last_name',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 250,
+      editable: true,
+    },
+    {
+      field: 'gender',
+      headerName: 'Gender',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 100,
+    },
+    {
+      field: 'ip_address',
+      headerName: 'IP Address',
+      width: 200,
+      editable: true,
+    },
+  ];
+
   return (
     <div>
       <input
@@ -28,17 +64,25 @@ function App() {
         placeholder="Search..."
         onChange={handleFilter}
       />
+    <div style={{ height: 700, width: '100%' }}>
+      <DataGrid
+        rows={filteredData}
+        columns={columns}
+        pageSize={10}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+{/* 
       {filteredData.length != 0 && (
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
             return (
-              <a className="dataItem" href={value.link} target="_blank">
-                <p>{value.first_name} {key}</p>
-              </a>
+                <p>{value.first_name} | {value.last_name} {key}</p>
             );
           })}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
